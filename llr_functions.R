6308c7d5cfd57a3cd0d05885d06d158977feae01
gyuri.kang <78900024+krorange@users.noreply.github.com>
2024-10-29 16:15:11 -0400

    edited llr_functions.R

--- llr_functions.R
+++ llr_functions.R
@@ -6,11 +6,9 @@ llr = function(x, y, z, omega) {
 
 # Compute f hat function:
 compute_f_hat = function(z, x, y, omega) {
-  Wz = diag(make_weight_matrix(z, x, omega))
+  Wz = make_weight_matrix(z, x, omega)
   X = make_predictor_matrix(x)
-  WX <- apply(X, 2, function(col) col * Wz)
-  Wy <- y * Wz
-  f_hat <- c(1, z) %*% solve(t(X) %*% WX) %*% t(X) %*% Wy
+  f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
   return(f_hat)
 }
 
